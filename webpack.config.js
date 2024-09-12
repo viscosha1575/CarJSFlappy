3a55d810b635988e6a2b56a3127c2fb4466ab7cc
viscosha1575 <61380433+viscosha1575@users.noreply.github.com>
2024-09-12 23:27:35 +0200

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  // Убедись, что путь правильный
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'development',
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 8080,
-  },
+    mode: 'development', // Убедитесь, что mode установлен
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    }
 };
